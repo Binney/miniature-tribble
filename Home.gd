9d5b4af253fa0467d8bc99cb1a5f280fff6fe697
Sarah Binney <sarah.binney@softwire.com>
2022-10-28 21:56:13 +0100

    first pass note from freq

--- Home.gd
+++ Home.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+const FToNote = preload("res://f_to_note.gd")
+
 var effect
 var recording
 var spectrum
@@ -31,18 +33,22 @@ func _on_PlayButton_pressed():
 	$AudioStreamPlayer.stream = recording
 	$AudioStreamPlayer.play()
 
-const VU_COUNT = 32
-const FREQ_MAX = 5050.0
+const VU_COUNT = 50
+const FREQ_MAX = 14080.0
 
 const WIDTH = 400
 const HEIGHT = 100
 
 const MIN_DB = 60
 
+var current_note
+
 func _draw():
 	#warning-ignore:integer_division
 	var w = WIDTH / VU_COUNT
 	var prev_hz = 0
+	var highest_magnitude = 0
+	var best_note = 440
 	for i in range(1, VU_COUNT+1):
 		var hz = i * FREQ_MAX / VU_COUNT;
 		var magnitude: float = spectrum.get_magnitude_for_frequency_range(prev_hz, hz).length()
@@ -50,7 +56,11 @@ func _draw():
 		var height = energy * HEIGHT
 		draw_rect(Rect2(w * i, HEIGHT - height, w, height), Color.white)
 		prev_hz = hz
-
+		if magnitude > highest_magnitude:
+			best_note = hz
+			highest_magnitude = magnitude
+	$Label.text = str(best_note) + 'Hz'
+	$NoteLabel.text = FToNote.freqToNote(best_note).to_string() + ' (I think)'
 
 func _process(_delta):
 	update()
---
+++ f_to_note.gd
@@ -0,0 +1,33 @@
+# https://pages.mtu.edu/~suits/NoteFreqCalcs.html
+
+const a = pow(2.0, 1.0 / 12.0)
+const A4 = 440 # Hz - middle C
+
+const MIN_FREQ = 27.5
+const MAX_FREQ = 14080
+
+const NOTES = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+
+class Note:
+	var oct: int
+	var note: String
+	func _init(newOct, newNote):
+		oct = newOct
+		note = newNote
+	func _to_string():
+		return note + str(oct)
+
+static func freqToNote(freq) -> Note:
+	assert(freq > MIN_FREQ, 'Frequency must be greater than 27.5Hz')
+	assert(freq < MAX_FREQ, 'Frequency must be less than 14080Hz')
+
+	# f(n) = f(0) * a^n
+	# ln(fn) = ln(f0) + n ln(a)
+	var n = (log(freq) - log(A4)) / log(a)
+	var steps_from_middle_c = stepify(n, 1.0)
+	
+	var oct = 4 + floor(steps_from_middle_c / 8)
+	var note_index = posmod(steps_from_middle_c, 8)
+	
+	#return NOTES[note_index] + str(oct)
+	return Note.new(oct, NOTES[note_index])
